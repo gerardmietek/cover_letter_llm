e3830f3585b8cf3e0c7b7f8057bde57dbd79c5c7
Gerard Miętek <gerardmietek@gmail.com>
2025-04-12 17:06:03 +0200

    form with no ai connected

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'ug791dvuhh-19efha-fafwaf3f0h1df'
+    app.config['SECRET_KEY'] = 'ug791dvuhh-19efha-fafwaf3f0h1df' # Klucz do sesji i ciasteczek
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    if request.method == 'POST':
+        name = request.form.get('name')
+        mail = request.form.get('mail')
+        num = request.form.get('num')
+        title = request.form.get('title')
+        cname = request.form.get('cname')
+        desc = request.form.get('desc')
+        amount = request.form.get('amount')
+
+        #prompt for later
+        prompt = f"""Write a cover letter based on this information:
+        Full name: {name},
+        E-mail: {mail},
+        Phone number: {num},
+        Job title of the person: {title},
+        Name of the company which the person chose: {cname},
+        Description of the job the person applies for: {desc},
+        Maximum cover letter length (in words): {amount}
+        """
+
+        
+        cover_letter = "RANDOM RESPONSE"
+
+        return render_template('index.html', cover_letter=cover_letter)
+
+    return render_template('index.html')
